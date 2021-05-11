64c47672337aa20ecd88f8704d7c045ad3b7cceb
KubMakCZ <KubMakCZ@gmail.com>
2021-05-11 20:27:03 +0200

    Update tester5.py

--- tester5.py
+++ tester5.py
@@ -1,6 +1,8 @@
 import nmap
 nm = nmap.PortScanner()
 
-nm.scan(hosts='192.168.0.0/24', arguments='-sS -O')
+#TCP SYN (Stealth) Scan (-sS) &&  -O Enable OS detection
+nm.scan(hosts='192.168.0.0/24', arguments='-sS -O') 
 for host in nm.all_hosts():
     print(host)
+print(nm.csv())
